aed4bbd3ee1b5cf486e94587a5923e6510bad474
Daniel Evans <danharibo@gmail.com>
2013-01-24 01:18:04 +0000

    Major update to use new dbot timer API + fixes

--- notifier.js
+++ notifier.js
@@ -1,8 +1,9 @@
 var request = require('request'),
-    jsdom = require('jsdom');
+    jsdom = require('jsdom'),
+    _ = require('underscore')._;
 
 var notifier = function(dbot) {
-
+    // Declare Protocols here.
     var protocols = {
         'cdr': {
             'are_equal': function(a, b) {
@@ -45,6 +46,7 @@ var notifier = function(dbot) {
 
     var announce = function(thing, msg) {
         var servers = thing.servers;
+        console.log('Announcing: ' + msg);
         for(var s in servers) {
             if(servers.hasOwnProperty(s)) {
                 for(var c = 0; c < servers[s].length; c++) {
@@ -55,9 +57,10 @@ var notifier = function(dbot) {
     }
 
     function process_data(thing, data) {
-        var updated = [];
-        var added = [];
+        var updated = []; var added = []; var removed = [];
         var handler = protocols[thing.type];
+
+        // Process the new data-set to see if there's anything new.
         for(var x = 0; x < data.length; x++) {
             var found = false;
             for(var y = 0; y < thing.item_cache.length; y++) {
@@ -69,68 +72,81 @@ var notifier = function(dbot) {
                     }
                 }
             }
+
             if(found === false) {
                 added.push(handler.printable(data[x]));
                 thing.item_cache.push(data[x]);
             }
         }
-        return { 'updated': updated, 'added': added };
+
+        for(var x = 0; x < thing.item_cache.length; x++) {
+            var found = false;
+            for(var y = 0; y < data.length; y++) {
+                if(handler.are_equal(data[y], thing.item_cache[x])) {
+                    found = true;
+                }
+            }
+
+            if(found === false) {
+                removed.push(thing.item_cache[x]);
+            }
+        }
+        return { 'updated': updated, 'added': added, 'removed': removed };
     }
 
     function poll(thing, no_changes) {
         var endpoint = thing.endpoint;
         var handler = protocols[thing.type];
         var ua = dbot.config.name + ' BOT notify module';
-        console.log('Polling ' + endpoint + ' (UA: ' + ua + ')');
-        request.get({
-            'url': endpoint,
-            'headers': {
-                'Host': 'repo.steampowered.com',
-                'User-Agent': ua
-            }
-        },
+        request.get({ 'url': endpoint, 'headers': _.defaults(thing.headers, { 'User-Agent': ua }) },
         function(error, response, body) {
             var data = handler.parse(body); 
             if(Array.isArray(data)) {
                 if( thing.item_cache.length == 0 ) {
                     announce(thing, 'Built initial cache with ' + data.length + ' items');
-                    process_data(thing,data);
+                    process_data(thing, data);
                 }  
-                else if( thing.item_cache.length < data.length ) {
+                else {
                     var res = process_data(thing, data);
-                    var output = '';
+                    var units = 0;
                     if( res.updated.length > 0 ) {
-                        output += res.updated.length;
-                        output += ' updated: ';
-                        output += res.updated.join(', ');
+                        announce(thing, res.updated.length + ' updated: ' 
+                                  + res.updated.join(', ') );
+                        units++;
                     }
                     if( res.added.length > 0 ) {
-                        output += res.added.length;
-                        output += ' added: ';
-                        output += res.added.join(', ');
+                        announce(thing, res.added.length + ' added: ' 
+                                  + res.added.join(', ') );
+                        units++;
                     }
-                    announce(thing, output);
-                }
-                else if(no_changes) {
-                    announce(thing, 'No new items (' + data.length + ' items)');
+                    if( res.removed.length > 0 ) {
+                        announce(thing, res.removed.length + ' added: ' 
+                                  + res.removed.join(', ') );
+                        units++;
+                    }
+                    if(units == 0 && no_changes) {
+                        announce(thing, 'No new items (' + data.length + ' items)');
+                    }
                 }
             }
             else {
-                announce(thing, 'Malformed response');
+                announce(thing, 'Malformed response (from ' + thing.endpoint + ')');
             }
         });
     }
 
     var watchers = [];
-    for(var i = 0; i < dbot.config.notifier.watches.length; i++) {
-        watchers[i] = dbot.config.notifier.watches[i];
-        watchers[i].check = function() {
-            poll(this);
-            dbot.timers.addOnceTimer(this.refresh, this.check.bind(this) );
-        }
-        watchers[i].item_cache = [];
-        watchers[i].check();
-    }
+    dbot.config.notifier.watches.forEach(function(watch) {
+        watchers.push(watch);
+        watch.item_cache = [];
+        watch = _.defaults(watch, { 
+            'headers': [],
+            'refresh': 60000  
+        });
+        dbot.api.timers.addTimer(watch.refresh, function() {
+            poll(watch);
+        });
+    });
 
     var commands = {
         '~checknow': function(event) {
