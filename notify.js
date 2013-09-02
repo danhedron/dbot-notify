8ec89e79676712f310ecd9835ba4a3dc569e5c5a
Daniel Evans <danharibo@gmail.com>
2013-09-02 01:05:00 +0000

    Refactored module to use new code

--- notify.js
+++ notify.js
@@ -1,189 +1,63 @@
-var request = require('request'),
-          _ = require('underscore')._;
+var _ = require('underscore')._,
+    notify_core = require('./notify_core'),
+    Entry = notify_core.Entry, Feed = notify_core.Feed,
+    Announcement = notify_core.Announcement,
+    Reminder = notify_core.Reminder;
 
 var notifier = function(dbot) {
-	var protocols = {
-		available: ['cdr', 'repo'],
-		loaded: {}
-	};
+  var feeds = [];
+  dbot.config.notify.feeds.forEach(function(feed) {
+    var f = new Feed(
+      dbot,
+      feed.name,
+      feed.protocol,
+      feed.endpoint,
+      feed.interval,
+      feed.options
+      );
 
-	function loadProtocol(name) {
-		if(!(_.contains(_.keys(protocols.loaded), name))) {
-			console.log('Loading protocol: ' + name);
-			var r = require('./protocols/'+name+'.js');
-			if(_.isObject(r)) {
-				protocols.loaded[name] = r;
-				return true;
-			}
-		}
-		return false;
-	}
+    if(feed.announcements) {
+      feed.announcements.forEach(function(announce) {
+        var a = new Announcement(
+          announce.channel,
+          announce.server,
+          announce.events
+          );
+        f.announcements.push(a);
+      });
+    }
 
-	function getProtocol(name) {
-		if(!_.contains(_.keys(protocols.loaded),name)) {
-			if(!loadProtocol(name)) {
-				return false;
-			}
-		}
-		return protocols.loaded[name];
-	}
-	
-	function announce(watcher, msg) {
-		var servers = watcher.servers;
-		console.log('Announcing: ' + msg);
-		for(var s in servers) {
-			if(servers.hasOwnProperty(s)) {
-				for(var c = 0; c < servers[s].length; c++) {
-					dbot.say(s, servers[s][c], msg);
-				}
-			}
-		}
-	}
+    if(feed.reminders) {
+      feed.reminders.forEach(function(remind) {
+        var r = new Reminder(
+          remind.channel,
+          remind.server,
+          remind.time
+          );
+        f.reminders.push(r);
+      });
+    }
 
-	function process_data(watcher, data) {
-		var updated = [],
-			added = [],
-			removed = [],
-			handler = getProtocol(watcher.protocol),
-			found = false,
-			x = 0, y = 0;
+    feeds.push(f);
 
-		// Process the new data-set to see if there's anywatcher new.
-		for(x = 0; x < data.length; x++) {
-			found = false;
-			for(y = 0; y < watcher.item_cache.length; y++) {
-				if(handler.are_equal(watcher.item_cache[y], data[x])) {
-					found = true;
-					if(handler.is_update(watcher.item_cache[y], data[x])) {
-						updated.push(handler.printable(data[x]));
-						watcher.item_cache[y] = data[x];
-					}
-				}
-			}
+    dbot.api.timers.addTimer(watch.refresh, function() {
+      f.poll(false);
+    });
+  });
 
-			if(found === false) {
-				added.push(handler.printable(data[x]));
-				watcher.item_cache.push(data[x]);
-			}
-		}
+  var commands = {
+    '~checkfeeds': function(event) {
+      for(var i = 0; i < feeds.length; i++) {
+        feeds[i].poll(true);
+      }
+    }
+  };
 
-		x = watcher.item_cache.length;
-
-		while(x--) {
-			found = false;
-			for(y = 0; y < data.length; y++) {
-				if(handler.are_equal(data[y], watcher.item_cache[x])) {
-					found = true;
-				}
-			}
-
-			if(found === false) {
-				removed.push(handler.printable(watcher.item_cache[x]));
-				watcher.item_cache.splice(x, 1);
-			}
-		}
-		return { 'updated': updated, 'added': added, 'removed': removed };
-	}
-
-	function process(watcher, no_changes, data) {
-		if(Array.isArray(data)) {
-			watcher.malformed_response = false;
-
-			if( watcher.item_cache.length === 0 ) {
-				process_data(watcher, data);
-			}
-			else {
-				var res = process_data(watcher, data);
-				var units = 0;
-				if( watcher.announce_updated && res.updated.length > 0 ) {
-					announce(watcher, '\x037' + watcher.updated_str + ' \x032('+res.updated.length+'):\x0F ' 
-							+ res.updated.join(', ') );
-					units++;
-				}
-				if( watcher.announce_added && res.added.length > 0 ) {
-					announce(watcher, '\x033' + watcher.added_str + ' \x032('+res.added.length+'):\x0F ' 
-							+ res.added.join(', ') );
-					units++;
-				}
-				if( watcher.announce_removed && res.removed.length > 0 ) {
-					announce(watcher, '\x034' + watcher.removed_str + ' \x032('+res.removed.length+'):\x0F ' 
-							+ res.removed.join(', ') );
-					units++;
-				}
-				if(units === 0 && no_changes) {
-					announce(watcher, 'No changes \x032(' + data.length + ' items)\x0F');
-				}
-			}
-		}
-		else if( watcher.malformed_response === false ) {
-			watcher.malformed_response = true;
-
-			announce(watcher, 'Malformed response - ' + watcher.endpoint);
-		}
-	}
-
-	function poll(watcher, no_changes) {
-		var endpoint = watcher.endpoint,
-			handler = getProtocol(watcher.protocol),
-			ua = dbot.config.name + ' BOT notify module';
-		
-		request.get({ 'url': endpoint, 'headers': _.defaults(watcher.headers, { 'User-Agent': ua }) },
-				function(error, response, body) {
-					try {
-					    handler.parse(body, function(data) {
-						    process(watcher, no_changes, data);
-					    });
-					}
-                    catch(e) {
-                        console.log('Error Parsing ' + endpoint);
-                        console.log(e);
-                    }
-				});
-	}
-
-	var watchers = [];
-	dbot.config.notify.watches.forEach(function(watch) {
-		watchers.push(watch);
-		watch.item_cache = [];
-		watch.malformed_response = false;
-		watch = _.defaults(watch, {
-			'headers': [],
-			'refresh': 60000,
-			'announce_added': true,
-			'announce_updated': true,
-			'announce_removed': true,
-			'added_str': 'New',
-			'updated_str': 'Updated',
-			'removed_str': 'Removed'
-		});
-		dbot.api.timers.addTimer(watch.refresh, function() {
-			poll(watch);
-		});
-		poll(watch); // poll instantly for initial cache, don't wait for timer
-	});
-
-	var commands = {
-		'~checknow': function(event) {
-			for(var i = 0; i < watchers.length; i++) {
-				poll(watchers[i], true);
-			}
-		},
-		'~forcenew': function(event) {
-			for(var i = 0; i < watchers.length; i++) {
-				if(watchers[i].item_cache.length > 0) {
-					watchers[i].item_cache.length = watchers[i].item_cache.length-1;
-				}
-			}
-		}
-	};
-
-	commands['~forcenew'].access = 'admin';
-
-	return {
-		'commands': commands
-	};
+  return {
+    'commands': commands
+  };
 };
 
 exports.fetch = function(dbot) {
-	return notifier(dbot);
+  return notifier(dbot);
 };
---
+++ notify_core.js
@@ -0,0 +1,195 @@
+function Entry(ID, Title, Timestamp)
+{
+  this.ID = ID;
+  this.Title = Title;
+  this.Timestamp = Timestamp;
+
+  this.isUpdate = function(other)
+  {
+    return (other.ID == this.ID) && (this.Timestamp != other.Timestamp);
+  }
+}
+
+function Announcement(Channel, Server, Events)
+{
+  this.channel = Channel;
+  this.server = Server;
+  this.events = Events;
+
+  this.announce_updated = (this.events.indexOf("updated") != -1);
+  this.announce_added = (this.events.indexOf("added") != -1);
+
+  var announce_queue = {"added":[], "updated":[]};
+
+  this.handleUpdate = function(entry)
+  {
+    if(this.announce_updated)
+    {
+      announce_queue.updated.push(entry.Title);
+    }
+  }
+
+  this.handleNew = function(entry)
+  {
+    if(this.announce_added)
+    {
+      announce_queue.added.push(entry.Title);
+    }
+  }
+
+  this.announce = function(dbot)
+  {
+    if(announce_queue.added.length > 0) {
+      dbot.send(Server, Channel, "Added: " + announce_queue.added.join(', '));
+      announce_queue.added.length = 0;
+    }
+    if(announce_queue.updated.length > 0) {
+      dbot.send(Server, Channel, "Updated: " + announce_queue.updated.join(', '));
+      announce_queue.updated.length = 0;
+    }
+  }
+}
+
+function Reminder(Channel, Server, Time)
+{
+  this.channel = Channel;
+  this.server = Server;
+  this.time = Time;
+
+  var remind_queue = [];
+
+  this.handleRemind = function(now, entry)
+  {
+    if(now + this.time > entry.Timestamp)
+    {
+      remind_queue.push(entry.Title);
+      return true;
+    }
+    return false;
+  }
+
+  this.remind = function(dbot)
+  {
+    if(remind_queue.length > 0) {
+      dbot.send(Server, Channel, "Only " + this.time + " seconds left until: " + remind_queue.join(', '));
+      remind_queue.length = 0;
+    }
+  }
+}
+
+function Feed(dbot, Name, Protocol, Endpoint, Interval, Options)
+{
+  this.name = Name;
+  this.protocol = Protocol;
+  this.endpoint = Endpoint;
+  this.interval = Interval;
+  this.options = Options;
+
+  // Load the protocol backend
+  var backend = require('./protocols/'+this.protocol);
+
+  this.poll = function(manual)
+  {
+    backend.poll(this.endpoint, this.options, Entry,
+                 function(error, entries) {
+                   if(! error) {
+                     this.processEntries(entries);
+                   }
+                   else {
+                     console.log("[Notify] Protocol Error:", error);
+                     if(manual) {
+                       // TODO: announce failure.
+                     }
+                   }
+                 }.bind(this));
+  }
+
+  /**
+   * Annoucements attached to this feed
+   */
+  this.announcements = [];
+
+  /**
+   * Reminders
+   */
+  this.reminders = [];
+
+  /**
+   * Cache of previous entries, indexed by Entry's ID
+   */
+  var entry_cache = {};
+
+  /**
+   * Cache of last reminder times.
+   */
+  var latest_reminders = {};
+
+  this.now = function()
+  {
+    return (new Date()).getTime()/1000;
+  }
+
+  var newEntry = (function(entry)
+                  {
+                    entry_cache[entry.ID] = entry;
+                    latest_reminders[entry.ID] = 0;
+                    for(var a = 0; a < this.announcements.length; ++a) {
+                      this.announcements[a].handleNew(entry);
+                    }
+                  }).bind(this);
+
+  var updateEntry = (function(entry)
+                     {
+                       for(var a = 0; a < this.announcements.length; ++a) {
+                         this.announcements[a].handleUpdate(entry, entry_cache[entry.ID]);
+                       }
+                       entry_cache[entry.ID] = entry;
+                     }).bind(this);
+
+  this.processEntries = function(entries)
+  {
+    for(var e = 0; e < entries.length; ++e)
+    {
+      console.log("Processing", entries[e].ID);
+      var entry = entries[e];
+      if(entry_cache[entry.ID])
+      {
+        if(entry_cache[entry.ID].isUpdate(entry))
+        {
+          updateEntry(entry);
+        }
+      }
+      else
+      {
+        newEntry(entry);
+      }
+
+      // Fire off reminders
+      for(var r = 0; r < this.reminders.length; ++r)
+      {
+        if(latest_reminders[entry.ID] < entry.Timestamp - this.reminders[r].time)
+        {
+          if(this.reminders[r].handleRemind(this.now(), entry))
+          {
+            latest_reminders[entry.ID] = this.now();
+          }
+        }
+      }
+    }
+
+    // Flush announcements.
+    for(var a = 0; a < this.announcements.length; ++a) {
+      this.announcements[a].announce(dbot);
+    }
+    for(var r = 0; r < this.reminders.length; ++r)
+    {
+      this.reminders[r].remind(dbot);
+    }
+  }
+}
+
+// Export
+exports.Entry = Entry;
+exports.Announcement = Announcement;
+exports.Reminder = Reminder;
+exports.Feed = Feed;
